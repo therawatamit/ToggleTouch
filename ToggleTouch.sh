d7d90da5fbb843ae54110b3be23526c21f0cc4c9
Amit Rawat <therawatamit@gmail.com>
2020-04-13 20:50:47 +0530

    remove need for toggletouch state file

--- ToggleTouch.sh
+++ ToggleTouch.sh
@@ -3,14 +3,14 @@ xiname="Touchscreen"
 if [ "$#" -eq 1 ]; then
     xiname=$1 #to avoid possible error due to touchscreen.state mismatch, program may have to be run repeatedly, upto 3 times - unmitigated issue
 fi
-CS=$(cat ~/.toggletouch/touchscreen.state)
 tsiid=$(xinput | grep -i $xiname | awk -F'id=' '{print $2}' | sed -r 's|^([^.]+).*$|\1|; s|^[^0-9]*([0-9]+).*$|\1|')
-if [ "$CS" = "OFF" ]; then
+CS=$(xinput --list-props $tsiid|grep 'Device Enabled'|awk -F ':\t' '{print $NF}')
+if [ "$CS" = "0" ]; then
     xinput enable $tsiid
     #echo "Touch screen is now enabled"
     echo "ON" > ~/.toggletouch/touchscreen.state
 fi
-if [ "$CS" = "ON" ]; then #could use else instead too, but this is more specific (secure?)
+if [ "$CS" = "1" ]; then #could use else instead too, but this is more specific (secure?)
     xinput disable $tsiid
     #echo "Touch screen is now disabled"
     echo "OFF" > ~/.toggletouch/touchscreen.state
